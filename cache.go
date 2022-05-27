62726a4ffec751a52462dd3d57143233b0c6370e
MikShma <67111851+MikShma@users.noreply.github.com>
2022-05-27 15:19:43 +0300

    Update cache.go

--- cache.go
+++ cache.go
@@ -2,22 +2,50 @@ package cache
 
 import "time"
 
+type MyStruct struct {
+	Value    string
+	Deadline time.Time
+}
+
 type Cache struct {
+	KeyValue map[string]MyStruct
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{KeyValue: make(map[string]MyStruct)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) ClearExpired() {
+	for i := range c.KeyValue {
+		if c.KeyValue[i].Deadline.Before(time.Now()) && !c.KeyValue[i].Deadline.IsZero() {
+			delete(c.KeyValue, i)
+		}
+	}
+}
+
+func (c Cache) Get(key string) (string, bool) {
+	v, ok := c.KeyValue[key]
+	c.ClearExpired()
+	if !ok {
+		return "", false
+
+	}
+	return v.Value, ok
 
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.KeyValue[key] = MyStruct{Value: value}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() (s []string) {
+	c.ClearExpired()
+	for i := range c.KeyValue {
+		s = append(s, i)
+	}
+	return s
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.KeyValue[key] = MyStruct{Value: value, Deadline: deadline}
 }
